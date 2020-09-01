e1f915a802010b7d6f3528341f04d9bb2bc1fb95
Anil Allewar <anilallewar@yahoo.co.in>
2020-09-01 22:18:32 +0530

    Using "exec" to execute the Java process from the bash itself - when launching java earlier it used to execute as a child process. Instead of creating a new process, "exec" will replace the shell process by the JVM.
    Processes created directly by Docker receive the TERM signal and it is their responsibility to properly stop running child processes. That is a problem if the actual server process is spawned by a shell script, as is often the case for a Java service. In this case the TERM signal is received by the shell process, but Bash will not forward that signal to the child process. This means that the shell process will stop, but the JVM will continue to run.

--- config-server/src/main/resources/run.sh
+++ config-server/src/main/resources/run.sh
@@ -4,4 +4,4 @@ SPRING_ACTIVE_PROFILE=${SPRING_ACTIVE_PROFILE:-"local-docker"}
 GIT_BRANCH_LABEL=${GIT_BRANCH_LABEL:-"develop"}
 
 # The environment variables are already set up by the Dockerfile
-java -Djava.security.egd=file:/dev/urandom -Dspring.profiles.active=${SPRING_ACTIVE_PROFILE} -Dgit.config.active.branch=${GIT_BRANCH_LABEL} -Duser.timezone=Asia/Kolkata $JAVA_OPTIONS -jar ${APP_JAR_NAME}-${APP_JAR_VERSION}.jar
+exec java -Djava.security.egd=file:/dev/urandom -Dspring.profiles.active=${SPRING_ACTIVE_PROFILE} -Dgit.config.active.branch=${GIT_BRANCH_LABEL} -Duser.timezone=Asia/Kolkata $JAVA_OPTIONS -jar ${APP_JAR_NAME}-${APP_JAR_VERSION}.jar
